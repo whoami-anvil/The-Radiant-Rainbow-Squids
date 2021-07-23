9fde3a951a4b7676070637e0fa121331ac39c0c3
Zade Micallef <zade.micallef@gmail.com>
2021-07-23 15:04:51 -0400

    Debugging 4

--- BWSI_BackSeat.py
+++ BWSI_BackSeat.py
@@ -68,9 +68,10 @@ class BackSeat ():
 
 			### These flags are for the test code. Remove them after the initial test!
 			engine_started = False
-			print(engine_started)
 
 			while True:
+
+				print("Hello")
 
 				now = datetime.datetime.utcnow().timestamp()
 				# delta_time can be used for time difference calculation in decide?
