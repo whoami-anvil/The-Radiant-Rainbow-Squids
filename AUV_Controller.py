db862801a2d585cebee489053d9d59960d0a6227
Zade Micallef <zade.micallef@gmail.com>
2021-07-26 11:23:46 -0400

    Updated Files

--- AUV_Controller.py
+++ AUV_Controller.py
@@ -10,98 +10,151 @@ import numpy as np
 
 class AUVController():
     def __init__(self):
-        
+
         # initialize state information
         self.__heading = None
-        self.__speed = None
-        self.__rudder = None
+        self.__speed = 750
+        self.__rudder = 0.0
+		self.__rudder_prev = 0.0
         self.__position = None
-        
+		self.__speed_knots = None
+		self.__speed_mps = None
+
         # assume we want to be going the direction we're going for now
         self.__desired_heading = None
-        
+		self.__time_list = []
+
     def initialize(self, auv_state):
         self.__heading = auv_state['heading']
-        self.__speed = auv_state['speed']
-        self.__rudder = auv_state['rudder']
         self.__position = auv_state['position']
-        
+
         # assume we want to be going the direction we're going for now
         self.__desired_heading = auv_state['heading']
 
-    ### Public member functions    
+		self.__time_list = []
+
+    ### Public member functions
+	def update_state (self, auv_state):
+
+		self.__heading = auv_state['heading']
+		self.__position = auv_state['position']
+
+		#used for keeping track of times in AUV
+		self.__time_list.append(auv_state['last_fix_time'])
+
     def decide(self, auv_state, green_buoys, red_buoys, sensor_type='POSITION'):
 
-        # update state information
-        self.__heading = auv_state['heading']
-        self.__speed = auv_state['speed']
-        self.__rudder = auv_state['rudder']
-        self.__position = auv_state['position']
-                
-        # determine what heading we want to go
-        if sensor_type.upper() == 'POSITION': # known positions of buoys
-            self.__desired_heading = self.__heading_to_position(green_buoys, red_buoys)
-        elif sensor_type.upper() == 'ANGLE': # camera sensor
-            self.__desired_heading = self.__heading_to_angle(green_buoys, red_buoys)
-        
-        # determine whether and what command to issue to desired heading               
-        cmd = self.__select_command()
-        
-        return cmd
-        
+        		#decide rudder angles
+				#figure out how to get it to move there based on its last rudder angle
+				# update state information
+				self.__heading = auv_state['heading']
+				self.__position = auv_state['position']
+
+				# determine what heading we want to go
+
+				print(f"Red Buoys: {red_buoys}\nGreen Buoys: {green_buoys}")
+				if (red_buoys == None or green_buoys == None):
+
+					return (self.__rudder_prev * -1), 750
+
+				elif sensor_type.upper() == 'POSITION': # known positions of buoys
+
+					print("Have seen bouys")
+
+					self.__desired_heading = self.__heading_to_position(green_buoys, red_buoys)
+
+				elif sensor_type.upper() == 'ANGLE': # camera sensor
+
+					print("Have seen bouys")
+
+					self.__desired_heading = self.__heading_to_angle(green_buoys, red_buoys)
+
+				# determine whether and what command to issue to desired heading
+				delta_rudder, new_engine_speed = self.__select_command()
+
+				self.__rudder_prev += delta_rudder
+
+				self.__speed = new_engine_speed
+				self.__speed_knots = self.__speed / 250
+				self.__speed_mps = self.__speed_knots * (1852 / 3600)
+
+				return delta_rudder, new_engine_speed
+
     # return the desired heading to a public requestor
     def get_desired_heading(self):
         return self.__desired_heading
-    
-    
+
+
     ### Private member functions
-        
+
     # calculate the heading we want to go to reach the gate center
     def __heading_to_position(self, gnext, rnext):
         # center of the next buoy pair
         gate_center = ((gnext[0]+rnext[0])/2.0, (gnext[1]+rnext[1])/2.0)
-        
+
         # heading to gate_center
         tgt_hdg = np.mod(np.degrees(np.arctan2(gate_center[0]-self.__position[0],
                                                gate_center[1]-self.__position[1]))+360,360)
-        
+
         return tgt_hdg
-    
+
     def __heading_to_angle(self, gnext, rnext):
         # relative angle to the center of the next buoy pair
         relative_angle = (gnext[0] + rnext[0]) / 2.0
-        
-        # heading to center of the next buoy pair        
+
+        # heading to center of the next buoy pair
         tgt_hdg = self.__heading + relative_angle
-        
+
         return tgt_hdg
 
     # choose a command to send to the front seat
     def __select_command(self):
-        # Unless we need to issue a command, we will return None
-        cmd = None
-        
-        # determine the angle between current and desired heading
-        delta_angle = self.__desired_heading - self.__heading
-        if delta_angle > 180: # angle too big, go the other way!
-            delta_angle = delta_angle - 360
-        if delta_angle < -180: # angle too big, go the other way!
-            delta_angle = delta_angle + 360
-        
-        # how much do we want to turn the rudder
-        ## Note: using STANDARD RUDDER only for now! A calculation here
-        ## will improve performance!
-        turn_command = "STANDARD RUDDER"
-        
-        # which way do we have to turn
-        if delta_angle>2: # need to turn to right!
-            if self.__rudder >= 0: # rudder is turning the other way!
-                cmd = f"RIGHT {turn_command}"
-        elif delta_angle<-2: # need to turn to left!
-            if self.__rudder <= 0: # rudder is turning the other way!
-                cmd = f"LEFT {turn_command}"
-        else: #close enough!
-            cmd = "RUDDER AMIDSHIPS"
-        
-        return cmd
-    
+
+		# Unless we need to issue a command, we will return None
+		turn_angle = None #for rudder
+		rpm_speed = 750 #for RPM, 500RPM/knot, up to 5 knots
+
+		# determine the angle between current and desired heading
+		delta_angle = self.__desired_heading - self.__heading
+
+		if delta_angle > 180: # angle too big, go the other way!
+
+			delta_angle = delta_angle - 360
+
+		if delta_angle < -180: # angle too big, go the other way!
+
+			delta_angle = delta_angle + 360
+
+		# with delta angle, match anything above zero to thirty degrees / 1.5 knots - anything above proportionally
+		# scales to 2 knots
+		if np.abs(delta_angle) > 10:
+
+			turn_angle = 15
+			rpm_speed = 1000
+
+		else:
+
+			turn_angle = 5
+			rpm_speed = 750
+
+		# which way do we have to turn
+		if delta_angle > 2: # need to turn to right!
+
+			if self.__rudder >= 0: # rudder is turning the other way!
+
+				pass
+
+		elif delta_angle < -2: # need to turn to left!
+
+			if self.__rudder <= 0: # rudder is turning the other way!
+
+				turn_angle = (turn_angle * -1)
+
+		else: #close enough!
+
+			turn_angle = 0
+
+		#adjust turn angle in comparison to previous rudder angle
+		rudder_turn = self.__rudder_prev - turn_angle
+
+		return rudder_turn, rpm_speed
--- BWSI_BackSeat.py
+++ BWSI_BackSeat.py
@@ -14,6 +14,7 @@ import threading
 import time
 import datetime
 
+import csv
 import utm
 
 from Image_Processor import ImageProcessor
@@ -22,6 +23,10 @@ from AUV_Controller import AUVController
 from pynmea2 import pynmea2
 import BluefinMessages
 from Sandshark_Interface import SandsharkClient
+
+log_file_name_initial = f"mission_logs/mission_{datetime.datetime.now()}.csv"
+log_file_write = open(log_file_name_initial, "w", encoding = 'UTF8', newline = '')
+writer = csv.DictWriter(log_file_write, fieldnames = ['Timestamp (UTC)', 'Position', 'Current Heading (deg)', 'Desired Heading (deg)', 'Green Bouys', 'Red Bouys', 'Error'])
 
 # Check the NMEA checksum
 def checkthesum(msg):
@@ -32,20 +37,21 @@ def checkthesum(msg):
         print(f"cmd = {cmd}\n")
         print(f"{expected} != {fields[1]}\n")
         return False
-    
+
     return True
 
 class BackSeat():
     # we assign the mission parameters on init
-    def __init__(self, host='localhost', port=8000, warp=1):
-        
+	def __init__ (self, host = 'localhost', port = 8000, warp = 1, log_file_name = "mission1.csv"):
+
         # back seat acts as client
         self.__client = SandsharkClient(host=host, port=port)
         self.__current_time = datetime.datetime.utcnow().timestamp()
         self.__start_time = self.__current_time
-        self.__log_file = f"backseat_{self.__start_time}.log"
+		self.__log_file_name = log_file_name
+		#self.__log_file = f"backseat_{self.__start_time}.log"
         self.__warp = warp
-        
+
         self.__auv_state = dict([
             ('position', (None, None)),
             ('latlon', None),
@@ -56,14 +62,15 @@ class BackSeat():
             ('pitch', None),
             ('last_fix_time', None)
             ])
-        
+
         # we'll use the first navigation update as datum
         self.__datum = None
-        
+		self.__datum_position = None
+
         # set to PICAM for the real camera
         self.__buoy_detector = ImageProcessor(camera='SIM')
         self.__autonomy = AUVController()
-    
+
     def run(self):
         try:
             # connect the client
@@ -75,19 +82,26 @@ class BackSeat():
 
             msg = BluefinMessages.BPLOG('ALL', 'ON')
             self.send_message(msg)
-                        
+
             ### These flags are for the test code. Remove them after the initial test!
             engine_started = False
             turned = False
             while True:
+				msgs = self.get_mail()
+
+				if ((self.__auv_state['heading'] == None) and (len(msgs) == 0)):
+
+					continue
+
                 now = datetime.datetime.utcnow().timestamp()
+				# delta_time can be used for time difference calculation in decide?
                 delta_time = (now-self.__current_time) * self.__warp
 
                 self.send_status()
 
                 self.__current_time += delta_time
-                
-                msgs = self.get_mail()
+
+
                 if len(msgs) > 0:
                     print("\nReceived from Frontseat:")
 
@@ -95,7 +109,7 @@ class BackSeat():
                         print(f"{str(msg, 'utf-8')}")
                         self.process_message(str(msg, 'utf-8'))
                         print(f"{self.__auv_state}")
-                        
+
 
                 ### ---------------------------------------------------------- #
                 ### Here should be the request for a photo from the camera
@@ -103,221 +117,206 @@ class BackSeat():
                 ###
                 ### Here you process the image and return the angles to target
                 ### green, red = self.__detect_buoys(img)
-                self.__buoy_detector.run(self.__auv_state)
+                red, green = self.__buoy_detector.run(self.__auv_state)
                 ### ---------------------------------------------------------- #
-                
-                
+
+
                 ### self.__autonomy.decide() probably goes here!
                 ### ---------------------------------------------------------- #
-                
-                ### turn your output message into a BPRMB request! 
+                print(f"Current Heading: {self.__auv_state['heading']}\nDesired Heading: {self.__autonomy.get_desired_heading()}")
+
+				writer.writerow({'Timestamp (UTC)' : datetime.datetime.utcnow(),
+								 'Position' : self.__auv_state['position'],
+								 'Current Heading (deg)' : self.__auv_state['heading'],
+								 'Desired Heading (deg)' : self.__autonomy.get_desired_heading(),
+								 'Green Bouys' : green,
+								 'Red Bouys' : red,
+								 'Error' : "None"})
+                ### turn your output message into a BPRMB request!
 
                 time.sleep(1/self.__warp)
 
-                
-                # ------------------------------------------------------------ #
-                # ----This is example code to show commands being issued
-                # ------------------------------------------------------------ #
-                if True:
-                    print(f"dt = {self.__current_time - self.__start_time}")
-                    if not engine_started and (self.__current_time - self.__start_time) > 3:
-                        ## We want to change the speed. For now we will always use the RPM (1500 Max)
-                        self.__current_time = datetime.datetime.utcnow().timestamp()
-                        # This is the timestamp format from NMEA: hhmmss.ss
-                        hhmmss = datetime.datetime.fromtimestamp(self.__current_time).strftime('%H%M%S.%f')[:-4]
+				if not(engine_started) and (self.__current_time - self.__start_time) > 0:
 
-                        cmd = f"BPRMB,{hhmmss},,,,750,0,1"
-                        # NMEA requires a checksum on all the characters between the $ and the *
-                        # you can use the BluefinMessages.checksum() function to calculate
-                        # and write it like below. The checksum goes after the *
-                        msg = f"${cmd}*{hex(BluefinMessages.checksum(cmd))[2:]}\n"
-                        self.send_message(msg)
-                        engine_started = True
+					## We want to change the speed. For now we will always use the RPM (1500 Max)
+					self.__current_time = datetime.datetime.utcnow().timestamp()
+					# This is the timestamp format from NMEA: hhmmss.ss
+					hhmmss = datetime.datetime.fromtimestamp(self.__current_time).strftime('%H%M%S.%f')[:-4]
 
-                    if not turned and (self.__current_time - self.__start_time) > 30:
-                        ## We want to set the rudder position, use degrees plus or minus
-                        ## This command is how much to /change/ the rudder position, not to 
-                        ## set the rudder
-                        self.__current_time = datetime.datetime.utcnow().timestamp()
-                        hhmmss = datetime.datetime.fromtimestamp(self.__current_time).strftime('%H%M%S.%f')[:-4]
+					cmd = f"BPRMB,{hhmmss},,,,750,0,1"
 
-                        cmd = f"BPRMB,{hhmmss},-15,,,750,0,1"
-                        msg = f"${cmd}*{hex(BluefinMessages.checksum(cmd))[2:]}\n"
-                        self.send_message(msg)
-                        turned = True
-                    
-                # ------------------------------------------------------------ #
-                # ----End of example code
-                # ------------------------------------------------------------ #
-                
-                
+					# NMEA requires a checksum on all the characters between the $ and the *
+					# you can use the BluefinMessages.checksum() function to calculate
+					# and write it like below. The checksum goes after the *
+					msg = f"${cmd}*{hex(BluefinMessages.checksum(cmd))[2:]}"
+					self.send_message(msg)
+
+					engine_started = True
+
+				else:
+
+					# Z - We need to add decide command and save outputs
+					delta_rudder, new_engine_speed = self.__autonomy.decide(self.__auv_state, red, green)
+
+					### turn your output message into a BPRMB request!
+
+					# Z - We need to save our output message
+					self.__current_time = datetime.datetime.utcnow().timestamp()
+					hhmmss = datetime.datetime.fromtimestamp(self.__current_time).strftime('%H%M%S.%f')[:-4]
+					cmd = f"BPRMB,{hhmmss},{delta_rudder},,,{new_engine_speed},0,1"
+					msg = f"${cmd}*{hex(BluefinMessages.checksum(cmd))[2:]}"
+					self.send_message(msg)
+
+				time.sleep(1 / self.__warp)
+
         except:
             self.__client.cleanup()
             client.join()
-          
-        
-    def process_message(self, message):
-        # DEAL WITH INCOMING BFNVG MESSAGES AND USE THEM TO UPDATE THE
-        # STATE IN THE CONTROLLER!
-        messages = message.split('$')
-        messages = messages[1:] # the first split will be blank, or partial message
-        
-        processed_list = list()
-        for msgpart in reversed(messages):
-            msg = f"${msgpart}"
-        
-            # JRE: skipping the checksum check for now!
-            with open(self.__log_file, 'a') as f:
-                f.write(f"{self.__current_time}, Processing: {msg}\n")
-    
-                if not checkthesum(msg):
-                    f.write("Checksum doesn't match!")
-                    print(f"Mismatched checksum, skipping message {msg}")
-                    return
-                
-            payld = msg.split('*')
-                    
-            fields = payld[0].split(',')
-            
-            # only process one of each type of message
-            if fields[0] in processed_list:
-                continue
-            
-            processed_list.append(fields[0])
-            
-            if fields[0] == '$BFNVG':
-                # don't care about message timestamp
-                #nvg_time = self.receive_nmea_time(fields[1])
-                        
-                # really only care about heading and position for now
-                self.__auv_state['latlon'] = self.receive_nmea_latlon(fields[2],fields[3], fields[4], fields[5])
-                            
-                if self.__datum is None:
-                    # on first navigation update, set datum
-                    self.__datum = self.__auv_state['latlon']
-                    self.__datum_position = utm.from_latlon(self.__datum[0], self.__datum[1])
-                    self.__auv_state['position'] = (0, 0)
-                else:
-                    self.__auv_state['position'] = self.__get_local_position()
-    
-                self.__auv_state['datum'] = self.__datum
-                self.__auv_state['altitude'] = float(fields[7])        
-                self.__auv_state['depth'] = float(fields[8])
-                self.__auv_state['heading'] = float(fields[9])
-                self.__auv_state['roll'] = float(fields[10])
-                self.__auv_state['pitch'] = float(fields[11])
-                self.__auv_state['last_fix_time'] = self.receive_nmea_time(fields[12])
-                with open(self.__log_file, 'a') as f:
-                    f.write(f"Interpreted as: {str(self.__auv_state)}\n")
-    
-            elif fields[0] == '$BFNVR':
-                nvr = {'timestamp': fields[1],
-                       'east_velocity': float(fields[2]),
-                       'north_velocity': float(fields[3]),
-                       'down_velocity': float(fields[4]),
-                       'pitch_rate': float(fields[5]),
-                       'roll_rate': float(fields[6]),
-                       'yaw_rate': float(fields[7]),
-                       }
-                with open(self.__log_file, 'a') as f:
-                    f.write(f"Interpreted as: {nvr}\n")
-                
-                
-            elif fields[0] == '$BFVER':
-                # don't care about the time for now
-                print(f"Version is {fields[2]}")
-                with open(self.__log_file, 'a') as f:
-                    f.write(f"Version: {fields[2]}\n")
-                
-            elif fields[0] == '$BFACK':
-                print(f"time = {fields[1]}")
-                msg_type = fields[2]
-                status = int(fields[5])
-                if status < 2:
-                    outstr = f"Vehicle failed to process request {msg_type}: {fields[7]}"
-                elif status == 2:
-                    outstr = f"Vehicle successfully processed request {msg_type}"
-                else:
-                    outstr = f"Request {msg_type} is pending"
-                with open(self.__log_file, 'a') as f:
-                    f.write(f"{outstr}")
-                
-                
-                
-            else:
-                print(f"I do not know how to process this message type: {fields[0]}")
-            
-        
-        
+
+
+	def process_message (self, msg):
+
+		# DEAL WITH INCOMING BFNVG MESSAGES AND USE THEM TO UPDATE THE
+		# STATE IN THE CONTROLLER!
+
+		# JRE: skipping the checksum check for now!
+
+		fields = msg.split(',')
+
+		if fields[0] == '$BFNVG':
+
+			# don't care about message timestamp
+			#nvg_time = self.receive_nmea_time(fields[1])
+
+			# really only care about heading and position for now
+			self.__auv_state['latlon'] = self.receive_nmea_latlon(fields[2],fields[3], fields[4], fields[5])
+
+			if self.__datum is None:
+
+				# on first navigation update, set datum
+				self.__datum = self.__auv_state['latlon']
+				self.__datum_position = utm.from_latlon(self.__datum[0], self.__datum[1])
+				self.__position = (0, 0)
+				self.__auv_state['position'] = (0, 0)
+
+			else:
+
+				self.__auv_state['position'] = self.__get_local_position()
+
+			self.__auv_state['datum'] = self.__datum
+			self.__auv_state['altitude'] = float(fields[7])
+			self.__auv_state['depth'] = float(fields[8])
+			self.__auv_state['heading'] = float(fields[9])
+			self.__auv_state['roll'] = float(fields[10])
+			self.__auv_state['pitch'] = float(fields[11])
+			fields2 = fields[12].split('*')
+			self.__auv_state['last_fix_time'] = self.receive_nmea_time(fields2[0])
+
+			self.__autonomy.update_state(self.__auv_state)
+
+		else:
+
+			#self.__log_error(f"Cannot process this message type: {fields[0]} (process_message)")
+
+			print(f"Cannot process this message type: {fields[0]}")
+
+
+
     def send_message(self, msg):
         with open(self.__log_file, 'a') as f:
             f.write(f"{self.__current_time}, Sending: {msg}\n")
 
         print(f"sending message {msg}...")
-        self.__client.send_message(msg)    
-        
-    def send_status(self):
-        #print("sending status...")
-        self.__current_time = datetime.datetime.utcnow().timestamp()
-        hhmmss = datetime.datetime.fromtimestamp(self.__current_time).strftime('%H%M%S.%f')[:-4]
-        msg = BluefinMessages.BPSTS(hhmmss, 1, 'BWSI Autonomy OK')
-        self.send_message(msg)
-            
+        self.__client.send_message(msg)
+
+	def send_status (self):
+
+		print("sending status...")
+		self.__current_time = datetime.datetime.utcnow().timestamp()
+		hhmmss = datetime.datetime.fromtimestamp(self.__current_time).strftime('%H%M%S.%f')[:-4]
+		msg = BluefinMessages.BPSTS(hhmmss, 1, 'BWSI Autonomy OK')
+		self.send_message(msg)
+
+
     def get_mail(self):
         msgs = self.__client.receive_mail()
         return msgs
-    
+
     def receive_nmea_time(self, hhmmss):
         tm = datetime.datetime.utcnow()
         nvg_time = datetime.datetime(tm.year,
                                      tm.month,
                                      tm.day,
-                                     int(hhmmss[0:2]), 
-                                     int(hhmmss[2:4]), 
+                                     int(hhmmss[0:2]),
+                                     int(hhmmss[2:4]),
                                      int(hhmmss[4:6]),
                                      0)
-        
+
         return nvg_time
-    
+
     def receive_nmea_latlon(self, latdeg, lathemi, londeg, lonhemi):
         latitude = int(latdeg[0:2]) + float(latdeg[2:]) / 60
         if lathemi == 'S':
             latitude = -latitude
-        
+
         longitude = int(londeg[0:3]) + float(londeg[3:]) / 60
         if lonhemi == 'W':
             longitude = -longitude
-            
+
         return (latitude, longitude)
-    
+
     def __get_local_position(self):
         # check that datum is in the same UTM zone, if not, shift datum
         local_pos = utm.from_latlon(self.__auv_state['latlon'][0],
                                     self.__auv_state['latlon'][1],
                                     force_zone_number=self.__datum_position[2],
                                     force_zone_letter=self.__datum_position[3])
-        
+
         return (local_pos[0]-self.__datum_position[0], local_pos[1]-self.__datum_position[1])
 
-    
-    
-    
-            
+def __log_error (self, error_msg):
+
+	log_file_write = open(self.__log_file_name, "w", encoding = 'UTF8', newline = '')
+	writer = csv.DictWriter(log_file_write, fieldnames = ['Timestamp (UTC)', 'Position', 'Current Heading (deg)', 'Desired Heading (deg)', 'Green Bouys', 'Red Bouys', 'Error'])
+	writer.writerow({'Timestamp (UTC)' : datetime.datetime.utcnow(),
+					 'Position' : None,
+					 'Current Heading (deg)' : None,
+					 'Desired Heading (deg)' : None,
+					 'Green Bouys' : None,
+					 'Red Bouys' : None,
+					 'Error' : error_msg})
+
+	self.__current_time = time.time()
+	hhmmss = datetime.datetime.fromtimestamp(self.__current_time).strftime('%H%M%S.%f')[:-4]
+	msg = BluefinMessages.BPABT(hhmmss, error_msg)
+	self.send_message(msg)
+
+
+
+
 def main():
+	writer.writeheader()
+
     if len(sys.argv) > 1:
+
         host = sys.argv[1]
     else:
+
         host = "localhost"
-        
+
     if len(sys.argv) > 2:
+
         port = int(sys.argv[2])
-    else:
-        port = 29500
-    
+
+	else:
+
+	    #port = 29500
+		port = 8042
+
     print(f"host = {host}, port = {port}")
-    backseat = BackSeat(host=host, port=port)
+    backseat = BackSeat(host = host, port = port, log_file_name = log_file_name_initial)
     backseat.run()
-    
-            
+
+
 if __name__ == '__main__':
     main()
